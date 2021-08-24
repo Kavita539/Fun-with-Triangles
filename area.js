756ace14036096d682c0a9d03b0d01f01b457d12
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-08-24 15:15:13 +0530

    added functions

---
+++ area.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll(".area-input");
+const areaBtn = document.querySelector("#area-btn");
+const areaOutput = document.querySelector("#output");
+
+function multiplyBaseAndHeight(base,height){
+    const multipliedOutput = base * height;
+	return multipliedOutput;
+}
+
+function calculateArea() {
+	//area = (base * height)/2
+	const basexheight = multiplyBaseAndHeight(Number(inputs[0].value), Number(inputs[1].value));
+	const area = basexheight / 2;
+	areaOutput.innerText = "The area of the triangle is " + area + " units²";
+}
+
+areaBtn.addEventListener('click', calculateArea);
+
+
---
+++ hypotenuse.js
@@ -0,0 +1,17 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn")
+const outputEl = document.querySelector("#output");
+
+function calculateSumOfsquares(a,b) {
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
+}
+
+function calculateHypotenuse() {
+    const sumOfSquares = calculateSumOfsquares(Number(sides[0].value),Number(sides[1].value));
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+    outputEl.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse + " units";
+
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
