04d4dc5d118220ba4b3a061b1399e0ed9f4201d2
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-08-27 01:02:02 +0530

    updated navbar

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled", "Isosceles"];
+const correctAnswers = ["90°", "right angled", "Isosceles", "60°"];
 
 function calculateScore() {
      let score = 0;
