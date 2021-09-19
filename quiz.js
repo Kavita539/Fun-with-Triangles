07830da696c5582a256b14c12de263f40b16ed30
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-09-19 20:21:01 +0530

    docs : updated code

--- quiz.js
+++ quiz.js
@@ -1,21 +1,30 @@
-const quizForm = document.querySelector(".quiz-form");
-const submitAnswerBtn = document.querySelector("#submit-answer-btn");
-const outputEl = document.querySelector("#output");
+let quizForm=document.querySelector(".quiz-form")
+const outputEl=document.querySelector("#output")
+const submitAnswerBtn=document.querySelector("#submit-answer-btn")
+const quizContainer=document.querySelectorAll(".question-container")
 
-const correctAnswers = ["65°", "right angled", "Isosceles", "60°", "Scalene", "Yes", "60°", "No"];
+let correctAns=["65°", "right angled", "Isosceles", "60°", "Scalene", "Yes", "60°", "No"];
 
-function calculateScore() {
-     let score = 0;
-     let index = 0;
-     const formResults = new FormData(quizForm);
-     for(let value of formResults.values()) {
-         if(value === correctAnswers[index]){
-             score = score + 1;
-         }
-         index = index + 1;
-     }
-     outputEl.innerText = "Your Score is " + score;
-     
-} 
+quizForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let index=0;
+    let score=0;
+    let formResults=new FormData(quizForm)
 
-submitAnswerBtn.addEventListener('click', calculateScore);
+    for(let eachEntry of formResults.values()){
+        if(eachEntry===correctAns[index]){
+            score++;
+            quizContainer[index].style.backgroundColor='#82d482'
+        }else{
+            quizContainer[index].style.backgroundColor='#ffabab'
+        }
+        index++
+    }
+    
+    outputEl.style.display='block';
+    // outputEl.innerText=`Your total score is ${score}/8`;
+    outputEl.innerText=`Your total score is ${score}/8\n\nThe Correct Answers are as given : 1)65°, 2)right angled, 3)Isosceles, 4)60°, 5)Scalene, 6)Yes, 7)60°, 8)No`
+    submitAnswerBtn.style.display='none'
+})
+
+
