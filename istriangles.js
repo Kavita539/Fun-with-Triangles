b577aefd261b8f1560ca6e00d63bcb2230e8aa5c
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-09-19 19:51:22 +0530

    updated code

--- istriangles.js
+++ istriangles.js
@@ -1,20 +1,22 @@
-const inputs = document.querySelectorAll(".angle-input");
-const isTriangleButton = document.querySelector("#is-triangle-btn");
-const outputE1 = document.querySelector("#output");
+var inputs = document.querySelectorAll(".angle-input");
+var isTriangleButton = document.querySelector("#is-triangle-btn");
+var outputE1 = document.querySelector("#output");
+var form =document.querySelector("#input-form");
 
-function calculateSumOfAngles(angle1, angle2, angle3) {
-    const sumOfAngles = angle1+angle2+angle3;
-    return sumOfAngles;
-}
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+        var firstAngle = Number(inputs[0].value);
+        var secondAngle = Number(inputs[1].value);
+        var thirdAngle = Number(inputs[2].value);
+        var sumOfAngles = firstAngle + secondAngle + thirdAngle;
+        if(sumOfAngles === 180){
+            outputE1.innerText = "Yay! The angles form a triangle";
+        }else{
+            outputE1.innerText = "Oops! The angles do not form a triangle";
+        }
+    })
+    
 
-function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-    if(sumOfAngles===180){
-        outputE1.innerText = "Yay! The angles form a triangle";
-    }else{
-        outputE1.innerText = "Oops! The angles do not form a triangle";
-    }
-   
-}
 
-isTriangleButton.addEventListener("click", isTriangle);
+
+
