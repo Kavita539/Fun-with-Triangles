f42be485b585f7487c10ede2ce7ac58566df957d
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-09-19 20:07:11 +0530

    docs : updated code

--- area.js
+++ area.js
@@ -1,19 +1,15 @@
 const inputs = document.querySelectorAll(".area-input");
 const areaBtn = document.querySelector("#area-btn");
 const areaOutput = document.querySelector("#output");
+var form =document.querySelector("#input-form");
 
-function multiplyBaseAndHeight(base,height){
-    const multipliedOutput = base * height;
-	return multipliedOutput;
-}
-
-function calculateArea() {
-	//area = (base * height)/2
-	const basexheight = multiplyBaseAndHeight(Number(inputs[0].value), Number(inputs[1].value));
-	const area = basexheight / 2;
-	areaOutput.innerText = "The area of the triangle is " + area + " units²";
-}
-
-areaBtn.addEventListener('click', calculateArea);
-
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	 var base = Number(inputs[0].value);
+	 var height = Number(inputs[1].value);
+	//  var basexheight = base * height;
+	 var area = (base * height)/2;
+	 areaOutput.innerText = "The area of the triangle is " + area.toFixed(2) + " units²";
+ })
+ 
 
